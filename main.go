22008607674b7d5b086a65731e8bea90c391cac3
Ross Gruber <ross.gruber@gmail.com>
2017-04-14 14:15:45 -0700

    Remove logs. Update stats struct to match v1.0.0-compat json obj

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -125,18 +124,15 @@ func main() {
 func fetchAndSetStats() {
 	for {
 		// Fetch stats
-		fmt.Println("Top of the loop")
 		stats, err := getNsqdStats(nsqdUrl)
 		if err != nil {
 			log.Fatal("Error scraping stats from nsqd: " + err.Error())
 		}
-		fmt.Println("Stats successfully scraped")
 
 		// Build list of detected topics and channels - the list of channels is built including the topic name that
 		// each belongs to, as it is possible to have multiple channels with the same name on different topics.
 		var detectedChannels []string
 		var detectedTopics []string
-		fmt.Println("Looking for dead topics")
 		for _, topic := range stats.Topics {
 			detectedTopics = append(detectedTopics, topic.Name)
 			for _, channel := range topic.Channels {
@@ -156,10 +152,8 @@ func fetchAndSetStats() {
 		knownTopics = detectedTopics
 		knownChannels = detectedChannels
 
-		fmt.Println("Setting metrics for: ")
 		// Loop through topics and set metrics
 		for _, topic := range stats.Topics {
-			fmt.Println("\t" + topic.Name)
 			paused := "false"
 			if topic.Paused {
 				paused = "true"
@@ -186,7 +180,6 @@ func fetchAndSetStats() {
 		}
 
 		// Scrape every scrapeInterval
-		fmt.Println("Finished... sleeping for " + strconv.Itoa(scrapeInterval))
 		time.Sleep(time.Duration(scrapeInterval) * time.Second)
 	}
 }
--- stats.go
+++ stats.go
@@ -35,12 +35,6 @@ import (
 	"net/http"
 )
 
-type statsResponse struct {
-	StatusCode int    `json:"status_code"`
-	StatusText string `json:"status_text"`
-	Data       stats  `json:"data"`
-}
-
 type stats struct {
 	Version   string   `json:"version"`
 	Health    string   `json:"health"`
@@ -96,9 +90,9 @@ func getNsqdStats(nsqdUrl string) (*stats, error) {
 	}
 	defer resp.Body.Close()
 
-	var sr statsResponse
-	if err = json.NewDecoder(resp.Body).Decode(&sr); err != nil {
+	var stats stats
+	if err = json.NewDecoder(resp.Body).Decode(stats); err != nil {
 		return nil, err
 	}
-	return &sr.Data, nil
+	return &stats, nil
 }
